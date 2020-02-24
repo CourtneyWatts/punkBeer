fcc53d1ca72a5c9f40df32eeddf40b5c9c397b4e
Courtney-Watts <courtney@siteglide.com>
2020-02-24 10:05:31 +0000

    Updates

---
+++ precache-manifest.ac0f82c43057fe5b26d08e65941662aa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "819ac43ba3b78849a1ef38c11ed68744",
+    "url": "/punkBeer/index.html"
+  },
+  {
+    "revision": "3dda32bec06c61860568",
+    "url": "/punkBeer/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "b7071ee798d2927a5c98",
+    "url": "/punkBeer/static/css/main.f52221db.chunk.css"
+  },
+  {
+    "revision": "3dda32bec06c61860568",
+    "url": "/punkBeer/static/js/2.04010dbf.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/punkBeer/static/js/2.04010dbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7071ee798d2927a5c98",
+    "url": "/punkBeer/static/js/main.f7486346.chunk.js"
+  },
+  {
+    "revision": "6bde4357a173f1b30cc4",
+    "url": "/punkBeer/static/js/runtime-main.7eb6e38d.js"
+  },
+  {
+    "revision": "27f4bfb4c3359183786ed3aee184d7ee",
+    "url": "/punkBeer/static/media/beers_banner.27f4bfb4.jpg"
+  }
+]);
